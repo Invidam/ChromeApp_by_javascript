4565b72a686a9260e0c56ffbe01c618846e169cf
Invidam <71889359+Invidam@users.noreply.github.com>
2021-06-01 10:18:56 +0900

    2일차 업데이트

--- Contents/index.js
+++ Contents/index.js
@@ -1,1 +1,32 @@
-console.log('Im working im js im hansu lol');
+/*
+//string
+const what = "Invidam"
+
+
+//boolean
+const wat = true;
+
+//number
+const wat = 123;
+
+//float
+const wat = 55.1
+
+
+const daysOfWeek = ['mon','tue','wed','thu','fri','sat','sun'];
+
+console.log(daysOfWeek);
+*/
+
+const myInfo = {
+  name : 'invidam',
+  age : 22,
+  gender : 'male',
+  isHandSome : true,
+  favFood: [
+    {name: 'kimchi', price: 10},
+    {name: 'chicken', price: 30}
+  ]
+}
+
+console.log(myInfo);
