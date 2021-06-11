93cceb802e47b4cf363c870885689a72413718ae
Invidam <71889359+Invidam@users.noreply.github.com>
2021-06-11 19:43:29 +0900

    cors 오류

--- Contents/weather.js
+++ Contents/weather.js
@@ -7,29 +7,19 @@ function getParticularMatter(lat, lng) {
   console.log("load");
   //경도 long 127 위도 lat 37
   console.log(lat,lng);
+  const headers = new Headers({
+  'Content-Type': 'text/xml',
+});
+fetch('https://evanmoon.tistory.com/rss', { headers });
   console.log(`https://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}`);
   fetch(`https://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}`,
     {
-         headers: { 'Content-Type': `*`,},
-         credentials: "include",
-       });
-      // {
-      //   method: "GET",
-      //   mode: 'cors',
-      //   headers: { 'Content-Type': 'application/json',}
-      // });
-    //   .then(function(response) {
-    //     console.log(response);
-    //     return response.json();
-    // }).then(function(json) {
-    //   console.log(json);;
-    // });
-    // .then(function(response) {
-    //   console.log(response);
-    // });
-    // .then(function(json) {
-    //   console.log("PM val: " + json);
-    // });
+      credentials: "include",
+    }).then(function(response) {
+      console.log(response);
+    }).then(function(json) {
+      console.log("PM val: " + json);
+    });
 }
 function getWeather(lat, lng) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
