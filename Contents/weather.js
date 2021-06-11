a096b7af9d5b6e2309a9a0b1f109d8630406e969
Invidam <71889359+Invidam@users.noreply.github.com>
2021-06-11 18:23:28 +0900

    좌표 위치 변환 기능 업데이트

--- Contents/weather.js
+++ Contents/weather.js
@@ -1,14 +1,43 @@
 const API_KEY = "5d77461c0704ff7b9103750e7b8cb292";
-const COORDS = "coords"
+const COORDS = "coords";
 const weather = document.querySelector(".js-weather");
+const PM_API_KEY = "B74412E0-BE78-3DBC-9F25-1EDE1666F169";
+function getParticularMatter(lat, lng) {
+
+  console.log("load");
+  //경도 long 127 위도 lat 37
+  console.log(lat,lng);
+  console.log(`http://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}`);
+  const Obj = fetch(`http://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}`,
+      {
+         mode : 'no-cors',
+         credentials: "include"
+      });
+    console.log(Obj.result);
+
+    // .then(function(response) {
+    //   console.log(response);
+    // });
+    // .then(function(json) {
+    //   console.log("PM val: " + json);
+    // });
+}
 function getWeather(lat, lng) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
     .then(function(response) {
       return response.json();
   }).then(function(json) {
+    //console.log(json);
     const temperature = json.main.temp;
     const place = json.name;
-    weather.innerText = `${temperature} @ ${place}`;
+    const weatherCondition = json.weather[0].description
+    weather.innerHTML = `
+    <ul>
+      <li><span>Location</span>: ${place}</li>
+      <li><span>Temperature</span>: ${temperature}℃</li>
+      <li><span>Weather</span>: ${weatherCondition} </li>
+    </ul>
+    `;
   });
 }
 function saveCoords(coordsObj) {
@@ -24,6 +53,7 @@ function handleGeoSuccess(position) {
   };
   saveCoords(coordsObj);
   getWeather(latitude,longitude);
+  getParticularMatter(latitude,longitude);
 }
 function handleGeoError() {
   console.log("Cant get geo-Location")
