821f74ee549ffac2469f4bc28c59a67b1fa4d4f3
Invidam <71889359+Invidam@users.noreply.github.com>
2021-06-11 18:44:55 +0900

    cors 오류

--- Contents/weather.js
+++ Contents/weather.js
@@ -7,14 +7,16 @@ function getParticularMatter(lat, lng) {
   console.log("load");
   //경도 long 127 위도 lat 37
   console.log(lat,lng);
-  console.log(`http://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}`);
-  const Obj = fetch(`http://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}`,
-      {
-         mode : 'no-cors',
-         credentials: "include"
+  console.log(`https://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}.json`);
+  fetch(`https://api.vworld.kr/req/address?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=${lng},${lat}&format=json&type=both&zipcode=true&simple=false&key=${PM_API_KEY}.json`,      {
+        credentials: "include"
       });
-    console.log(Obj.result);
-
+    //   .then(function(response) {
+    //     console.log(response);
+    //     return response.json();
+    // }).then(function(json) {
+    //   console.log(json);;
+    // });
     // .then(function(response) {
     //   console.log(response);
     // });
@@ -25,9 +27,10 @@ function getParticularMatter(lat, lng) {
 function getWeather(lat, lng) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
     .then(function(response) {
+      console.log(response);
       return response.json();
   }).then(function(json) {
-    //console.log(json);
+    console.log(json);
     const temperature = json.main.temp;
     const place = json.name;
     const weatherCondition = json.weather[0].description
